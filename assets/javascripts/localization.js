3b991be0952403cb544d904075af248cf5fcd1ac
Keran Kocher <kocher.ke@gmail.com>
2016-10-04 10:37:07 +0300

    i18n

--- assets/javascripts/localization.js
+++ assets/javascripts/localization.js
@@ -4,14 +4,20 @@ var uri = new URI(window.location.href);
 var locale = uri.search(true).locale;
 var file = document.body.dataset.i18n;
 
+if(locale !== undefined) {
+  setCookie("locale", locale, 365);
+} else {
+  locale = getCookie("locale");
+}
+
 var opts = { pathPrefix: ("./locales/" + locale), language: locale };
 if(locale !== undefined) { $localized.localize(file, opts).localize("header", opts).localize("footer", opts); };
 
 document.onclick = function(e) {
   var locale = e.target.dataset.lang;
-  // reload the page if the default language is wanted. Remove the locale param.
   if(locale === "bg") {
     window.location.href = uri.removeSearch("locale");
+    document.cookie = "locale=; expires=Thu, 01 Jan 1970 00:00:00 UTC"; // delete the cookie
   } else if (locale !== undefined) {
     window.location.href = uri.setSearch("locale", locale);
   };
